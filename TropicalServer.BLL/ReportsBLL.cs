cd9ef08f89df12cb6027d070d91cd3ad8078cf0b
roiceliu <roiceliu@birdwellsolutions.com>
2021-04-23 12:43:09 -0500

    Add project files.

---
+++ TropicalServer.BLL/ReportsBLL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using TropicalServer.DAL;//need to import DAL to use its funcitions
+
+namespace TropicalServer.BLL
+{
+    public class ReportsBLL
+    {
+       public Boolean IsValidUser(string UserName, string Password)
+        {
+            DataSet ds = GetUsersSetting_BLL();
+            Console.WriteLine(ds.Tables);
+            return true;
+        }
+
+        public DataSet GetProductByProductCategory_BLL(string newItemDescription)
+        {
+            return (new ReportsDAL().GetProductByProductCategory_DAL(newItemDescription));
+        }
+
+        public DataSet GetCustSalesRepNumber_BLL(int newCustSaleRepNum)
+        {
+            return (new ReportsDAL().GetCustSalesRepNumber_DAL(newCustSaleRepNum));
+        }
+
+        public DataSet GetUsersSetting_BLL()
+        {
+            return (new ReportsDAL().GetUsersSetting_DAL());
+        }
+
+        public DataSet GetCustomersSetting_BLL()
+        {
+            return (new ReportsDAL().GetCustomersSetting_DAL());
+        }
+
+        public DataSet GetPriceGroupSetting_BLL()
+        {
+            return (new ReportsDAL().GetPriceGroupSetting_DAL());
+        }
+
+        public DataSet GetRouteInfo_BLL(int newRouteID)
+        {
+            return (new ReportsDAL().GetRouteInfo_DAL(newRouteID));
+        }
+        public DataSet GetItemTypeID_BLL()
+        {
+            return (new ReportsDAL().GetItemTypeID_DAL());
+        }
+
+        public DataSet GetItemsDetail_BLL(string itemType)
+        {
+            return (new ReportsDAL().GetItemsDetail_DAL(itemType));
+        }
+
+        public DataSet Sample_BLL()
+        {
+            return (new ReportsDAL().Sample_DAL());
+        }
+    }
+}
---
+++ TropicalServerApp/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using TropicalServerApp.Models;
+using TropicalServer.BLL;
+
+
+namespace TropicalServerApp.Controllers
+{
+    public class UserAccountBinder : IModelBinder
+    {
+        //bind the input from user account to UserAccount object
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            HttpContextBase objContext = controllerContext.HttpContext;
+            string userName = objContext.Request["userName"];
+            string userPassword = objContext.Request["userPassword"];
+            UserAccount user = new UserAccount
+            {
+                UserName = userName,
+                UserPassword = userPassword
+            };
+
+            return user;
+        }
+    }
+
+    public class LoginController : Controller
+    {
+        // static user object 
+        // GET: Login
+        public ActionResult Login()
+        { 
+            return View();
+        }
+
+        //get all user filled data from Login form: userName & userpassword => check if correct
+        // public ActionResult SubmitLogin([ModelBinder(typeof(UserAccountBinder))] UserAccount obj)
+        public ActionResult SubmitLogin(UserAccount obj)
+        {
+            ReportsBLL Bll = new ReportsBLL();
+            DataSet ds = Bll.GetUsersSetting_BLL();
+            Console.WriteLine("getting user setting" + ds);
+            //Bll.IsValidUser(obj.UserName, obj.UserPassword);
+            return View("LoginResult", obj);
+        }
+
+    }
+}
